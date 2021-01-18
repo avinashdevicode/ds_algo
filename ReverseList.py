b9fd1789daf22687ff4e65ee10a9ab5722b1cf07
avinashdevicode <avinashdevicode@gmail.com>
2021-01-18 21:34:10 +0000

    reverse

--- ReverseList.py
+++ ReverseList.py
@@ -1,1 +1,13 @@
-import math
+def reverse(lis: list) -> list:
+    start_index = 0
+    end_index = len(lis) - 1
+    while start_index < end_index:
+        lis[start_index], lis[end_index] = lis[end_index], lis[start_index]
+        start_index += 1
+        end_index -= 1
+
+
+if __name__ == "__main__":
+    n = [1,2,3, 4]
+    reverse(n)
+    print(n)
